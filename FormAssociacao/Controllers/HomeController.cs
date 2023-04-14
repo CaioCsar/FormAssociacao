224cd82d65350c648e372e1b7e774869a4fd4e82
Caio <caiocsarbezerra95@gmail.com>
2023-04-13 21:29:59 -0300

    inicio

--- FormAssociacao/Controllers/HomeController.cs
+++ FormAssociacao/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace FormAssociacao.Controllers
             return View();
         }
 
+        public IActionResult FormAssociacao()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
